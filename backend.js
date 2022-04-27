fe07fa18876b7106e20558523a4154b488a37f7b
git-coder257 <85864939+git-coder257@users.noreply.github.com>
2022-04-27 08:33:02 +0100

    Create backend.js

---
+++ backend.js
@@ -0,0 +1,23 @@
+import express, { response } from "express"
+import pkg from 'pg';
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: "localhost",
+  user: "postgres",
+  password: "password",
+  database: "orsomworld",
+  port: "5433",
+})
+
+const query = (text, params) => pool.query(text, params)
+
+app.get("/users", async(req, res) => {
+    
+})
